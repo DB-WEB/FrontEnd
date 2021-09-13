cdc25a57516dce61f72ddd583c1807c85ba8ac2c
DB-WEB <85535584+DB-WEB@users.noreply.github.com>
2021-09-13 15:08:05 +0800

    f

--- FaceDetective.py
+++ FaceDetective.py
@@ -13,14 +13,14 @@ class ScreenVideoControl(object):
     def __init__(self):
         self.fps = 25  # 帧率为25，可以调节
         pyautogui.FAILSAFE = False
-        self.save_dir = 'C:\\Users\\Administrator\\Desktop\\Matchs\\Vadio'
+        self.save_dir = 'Vadio'
+        self.imgCut = 'ShootCut'
         self.screen_file_path = None
         self.get_video_path()
         self.screen_width = GetSystemMetrics(0)
         self.screen_height = GetSystemMetrics(1)
-        self.ScreemAear=ScreemAear()
-        self.threadContainer=[]
-        
+        self.ScreemAear = ScreemAear()
+        self.threadContainer = []
 
         self.face_detection = cv2.CascadeClassifier(
             'C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\cv2\data\\haarcascade_frontalface_default.xml')
@@ -36,8 +36,25 @@ class ScreenVideoControl(object):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_detection.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
-            mat=np.mat([[x,y],[w,h]])
-            t= self.ScreemAear.drawFace(x, y, w, h)
+            self.ScreemAear.drawFace(x, y, w, h)
+            files = os.listdir(self.imgCut)
+            crop = img[y-10:h+y+10, x-10:w+x+10]
+            if(len(files) == 0):
+                cv2.imwrite(os.path.join(
+                    self.imgCut, self.generate_img_name()), crop)
+            maxScore = 0
+            # 找到最像得那种照片
+            files = os.listdir(self.imgCut)
+            for file in files:
+                if os.path.isfile(os.path.join(self.imgCut, file)):
+                    template = cv2.imread(os.path.join(self.imgCut, file))
+                    score = self.match_img(crop, template)
+                    if(score > maxScore):
+                        maxScore = score
+            print(maxScore)
+            if(maxScore < 0.8):
+                cv2.imwrite(os.path.join(
+                    self.imgCut, self.generate_img_name()), crop)
 
             #template = cv2.imread('test.jpg')
             #self.match_img(img, template, 0.9)
@@ -46,9 +63,9 @@ class ScreenVideoControl(object):
 
     def video_record(self):
         print("screen record is doing........")
-       
         while True:
-            im = ImageGrab.grab(self.getBox()) #(left_x, top_y, right_x, bottom_y)
+            # (left_x, top_y, right_x, bottom_y)
+            im = ImageGrab.grab(self.getBox())
             # 转为opencv的BGR格式
             imm = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
             self.detectFace(imm)
@@ -68,34 +85,27 @@ class ScreenVideoControl(object):
             '%Y-%m-%d %H-%M-%S') + '_screen.avi'
         # 文件路径
         self.screen_file_path = os.path.join(self.save_dir, file_name)
-    
+
     def generate_img_name(self):
         return datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')+'.png'
 
-    #匹配要找得人脸
-    def match_img(self, image, template, value):
+    # 人脸是否存在
+    def match_img(self, image, template):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
         w, h = template_gray.shape[::-1]
         res = cv2.matchTemplate(
             gray_image, template_gray, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        if max_val >= value:
-            top_left = max_loc
-            cv2.rectangle(image, top_left,(top_left[0]+w, top_left[1]+h), 255, 2)
-            cv2.putText(image,"MR-XIAO-Ming",top_left,cv2.FONT_HERSHEY_SIMPLEX,0.2,(0,0,255))
-            cv2.imwrite(self.generate_img_name(), image)
-            cv2.imshow(self.generate_img_name(),image)
-            cv2.waitKey(0)
+        return max_val
 
     def getBox(self):
-        bbox =(self.screen_width/2,0,self.screen_width,self.screen_height/2)
+        bbox = (self.screen_width/2, 0, self.screen_width, self.screen_height/2)
         return bbox
+
 
 screen_video = ScreenVideoControl()
 screen_video.run()
-
-
 
 
 # corp
--- drawRectangle.py
+++ drawRectangle.py
@@ -1,31 +1,39 @@
-import win32gui, win32api
+import win32gui
+import win32api
 from win32api import GetSystemMetrics
 import threading
 import keyboard
+
 
 class ScreemAear:
     def __init__(self):
         self.dc = win32gui.GetDC(0)
         self.monitor = (0, 0, GetSystemMetrics(0), GetSystemMetrics(1))
 
-        self.red = win32api.RGB(0, 0, 0) # Red
-        self.green = win32api.RGB(0,255,0) 
-        self.width=GetSystemMetrics(0)
-        self.height=GetSystemMetrics(1)
+        self.red = win32api.RGB(0, 0, 0)  # Red
+        self.green = win32api.RGB(0, 255, 0)
+        self.width = GetSystemMetrics(0)
+        self.height = GetSystemMetrics(1)
 
     def DrawRectangle(self):
         for x in range(int(self.width/2)):
-                win32gui.SetPixel(self.dc,int(self.width/2)+x,0, self.red)  # draw red at 0,0
-                win32gui.SetPixel(self.dc,int(self.width/2)+x,int(self.height/2) ,self.red)# y,x
+            win32gui.SetPixel(self.dc, int(self.width/2)+x,
+                              0, self.red)  # draw red at 0,0
+            win32gui.SetPixel(self.dc, int(self.width/2)+x,
+                              int(self.height/2), self.red)  # y,x
         for y in range(int(self.height/2)):
-            win32gui.SetPixel(self.dc,int(self.width/2),y, self.red)  # draw red at 0,0
-            win32gui.SetPixel(self.dc,self.width-2,y ,self.red)# y,x
+            win32gui.SetPixel(self.dc, int(self.width/2), y,
+                              self.red)  # draw red at 0,0
+            win32gui.SetPixel(self.dc, self.width-2, y, self.red)  # y,x
 
-    def drawFace(self,x, y, w, h):
-        for pix in range(w):
-                win32gui.SetPixel(self.dc,int(self.width/2)+ x+pix,y, self.green) 
-                win32gui.SetPixel(self.dc,int(self.width/2)+ x+pix,y+h, self.green) 
-        for pix in range(h):
-            win32gui.SetPixel(self.dc,int(self.width/2)+ x,y+pix, self.green) 
-            win32gui.SetPixel(self.dc,int(self.width/2)+ x+w,y+pix, self.green) 
-       
+    def drawFace(self, x, y, w, h):
+        for pix in range(w+40):
+            win32gui.SetPixel(self.dc, int(self.width/2) -
+                              20 + x+pix, y-20, self.green)
+            win32gui.SetPixel(self.dc, int(self.width/2) -
+                              20 + x+pix, y+h+20, self.green)
+        for pix in range(h+40):
+            win32gui.SetPixel(self.dc, int(self.width/2) +
+                              x-20, y+pix-20, self.green)
+            win32gui.SetPixel(self.dc, int(self.width/2) +
+                              x+w+20, y+pix-20, self.green)
